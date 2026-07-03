6428089b77455ff57a8c9949a898c6f0fd1e0ec7
lex <lex@work.cn>
2026-07-03 18:31:59 +0800

    poste

--- lua/custom/plugins/poste_local.lua
+++ lua/custom/plugins/poste_local.lua
@@ -7,6 +7,7 @@ vim.pack.add {
   utils.gh('saghen/blink.cmp'),
 }
 
-vim.pack.add { vim.fs.normalize(os.getenv("HOME") .. "/ai/projects/poste") }
+-- 本地插件：直接添加到 runtimepath
+vim.opt.runtimepath:append(vim.fs.normalize(os.getenv("HOME") .. "/ai/projects/poste"))
 
 require("poste").setup()
