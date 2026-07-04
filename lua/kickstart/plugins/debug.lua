e1793c0a3cd171d097fc3672f67722585ed81bb8
lex <lex@work.cn>
2026-07-04 08:13:08 +0800

    debug lua in neovim

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -13,17 +13,25 @@ vim.pack.add {
   'https://github.com/mason-org/mason.nvim',
   'https://github.com/jay-babu/mason-nvim-dap.nvim',
   'https://github.com/leoluz/nvim-dap-go',
+  -- Lua debugger: debug Lua code running inside Neovim
+  'https://github.com/jbyuki/one-small-step-for-vimkind',
 }
 
+
 -- Basic debugging keymaps, feel free to change to your liking!
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'Debug: Start/Continue' })
-vim.keymap.set('n', '<F1>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
-vim.keymap.set('n', '<F2>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
+-- over , into, out , start, 
+-- F1   , F2  , F3  , F4
+-- stop , ui
+-- dx   , du
+vim.keymap.set('n', '<F1>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
+vim.keymap.set('n', '<F2>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
 vim.keymap.set('n', '<F3>', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
+vim.keymap.set('n', '<F4>', function() require('dap').continue() end, { desc = 'Debug: Start/Continue' })
+vim.keymap.set('n', '<leader>dx', function() require('dap').stop() end, { desc = 'Debug: Stop' })
 vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end, { desc = 'Debug: Toggle Breakpoint' })
 vim.keymap.set('n', '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, { desc = 'Debug: Set Breakpoint' })
 -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-vim.keymap.set('n', '<F7>', function() require('dapui').toggle() end, { desc = 'Debug: See last session result.' })
+vim.keymap.set('n', '<leader>du', function() require('dapui').toggle() end, { desc = 'Debug: See last session result.' })
 
 local dap = require 'dap'
 local dapui = require 'dapui'
@@ -72,14 +80,14 @@ dapui.setup {
 -- Change breakpoint icons
 -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
 -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
--- local breakpoint_icons = vim.g.have_nerd_font
---     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
---   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
--- for type, icon in pairs(breakpoint_icons) do
---   local tp = 'Dap' .. type
---   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
---   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
--- end
+local breakpoint_icons = vim.g.have_nerd_font
+    and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+  or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+for type, icon in pairs(breakpoint_icons) do
+  local tp = 'Dap' .. type
+  local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+  vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+end
 
 dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 dap.listeners.before.event_terminated['dapui_config'] = dapui.close
@@ -93,3 +101,33 @@ require('dap-go').setup {
     detached = vim.fn.has 'win32' == 0,
   },
 }
+
+-- ============================================================================
+-- Lua Debugger Configuration
+-- ============================================================================
+-- Uses one-small-step-for-vimkind to debug Lua code running inside Neovim.
+-- Great for debugging init.lua, plugins, or any Lua scripts.
+
+-- Register the nlua adapter for nvim-dap
+dap.adapters.nlua = function(callback, config)
+  callback({
+    type = 'server',
+    host = config.host or '127.0.0.1',
+    port = config.port or 8086,
+  })
+end
+
+---@diagnostic disable-next-line: missing-fields
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = 'Attach to running Neovim instance',
+    host = '127.0.0.1',
+    port = 8086,
+  },
+}
+
+-- No setup() needed for osv.
+-- To start debugging: :lua require('osv').launch()
+-- Then press <F4> to attach.
