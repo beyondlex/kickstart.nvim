5a9d3710b9163eaeaac06bad1d76192c8323acd5
lex <lex@work.cn>
2026-07-04 20:43:35 +0800

    feat: restore cursor position on file open

--- init.lua
+++ init.lua
@@ -252,6 +252,19 @@ do
     callback = function() vim.hl.on_yank() end,
   })
 
+  -- Restore cursor position when reopening files
+  vim.api.nvim_create_autocmd('BufReadPost', {
+    desc = 'Restore cursor position from last edit',
+    group = vim.api.nvim_create_augroup('kickstart-restore-cursor', { clear = true }),
+    callback = function()
+      local mark = vim.api.nvim_buf_get_mark(0, '"')
+      local lcount = vim.fn.line('$')
+      if mark[1] > 1 and mark[1] <= lcount then
+        pcall(vim.api.nvim_win_set_cursor, 0, { mark[1], mark[2] })
+      end
+    end,
+  })
+
   do
     require('custom.keymaps')
   end
