5e587d668897b36ba9c6eb94e8dd3ff0aea97aca
lex <lex@work.cn>
2026-07-07 14:19:45 +0800

    oil.nvim setup

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,4 +1,11 @@
 
 vim.pack.add { 'https://github.com/stevearc/oil.nvim' }
 
-require('oil').setup()
+require('oil').setup({
+	columns = {
+		"icon",
+		"permissions",
+		"size",
+		"mtime",
+	}
+})
